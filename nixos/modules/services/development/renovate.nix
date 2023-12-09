d35fc47dda9d96f60cb1a7fbdc8bf5170f9abb5e
Peter Lehmann <peter@lehmann.de>
2023-12-09 23:48:50 +0100

    Complete module

--- nixos/modules/services/development/renovate.nix
+++ nixos/modules/services/development/renovate.nix
@@ -2,18 +2,38 @@
 
 let
   cfg = config.services.renovate;
-  settingsFormat = pkgs.format.json {};
-  configFile = settingsFormat.generate "config.json" cfg.config;
+  settingsFormat = pkgs.format.json { };
+  settingsFile = settingsFormat.generate "config.json" cfg.settings;
 in {
+  inherit (pkgs.renovate) meta.maintainers;
+
   options = {
     services.renovate = {
       enable = mkEnableOption (lib.mdDoc "Renovatebot");
 
-      config = lib.mkOption {
-        default = null;
+      environmentFiles = lib.mkOption {
+        type = lib.types.listOf.path;
+        default = [ ];
+        example = [ /etc/renovate/environment.env ];
+        description = lib.mdDoc ''
+          Set environment files for renovate.
+        '';
+      };
+
+      path = lib.mkOption {
+        type = lib.types.listOf lib.types.packages;
+        default = [ ];
+        example = [ "pkgs.poetry" ];
+        description = lib.mdDoc ''
+          Renovate needs access to language specific tooling to perform it's tasks.
+        '';
+      };
+
+      settings = lib.mkOption {
+        default = { };
         type = settingsFormat.type;
         description = lib.mdDoc ''
-          Set options for renovate.
+          Set settings for renovate.
           See https://docs.renovatebot.com/self-hosted-configuration/ for possible values
         '';
       };
@@ -22,14 +42,19 @@ in {
 
   config = lib.mkIf cfg.enable {
     systemd.services.renovate = {
-      description = "renovate service";
-      environment = { RENOVATE_CONFIG_FILE = configFile };
+      wants = [ "network.target" ];
+      after = [ "network.target" ];
+      wantedBy = [ "multi-user.target" ];
+      description = "Renovate Service";
+      path = cfg.path;
+      environment = { RENOVATE_CONFIG_FILE = settingsFile };
       serviceConfig = {
+        DynamicUser = true;
+        environmentFile = cfg.environmentFiles;
         ExecStart = "${pkgs.renovate}/bin/renovate";
-        PrivateTmp = true;
-        ProtectSystem = "strict";
-        ProtectHome = "read-only";
+        Type = "simple";
         Restart = "on-failure";
+        RestartSec = 15;
       };
     };
   };
