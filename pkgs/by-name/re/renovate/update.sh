9a3f1960cc39bad75f2bb2e5ce3151efe546f1c6
Peter Lehmann <peter@lehmann.de>
2023-12-09 22:48:24 +0100

    update.sh: Add pre-clone check and printing

--- pkgs/by-name/re/renovate/update.sh
+++ pkgs/by-name/re/renovate/update.sh
@@ -10,6 +10,11 @@ tempDir=$(mktemp -d)
 latestTag=$(curl ${GITHUB_TOKEN:+" -u \":$GITHUB_TOKEN\""} "https://api.github.com/repos/renovatebot/renovate/releases/latest" | jq -s '.[0].tag_name' | tr -d '"')
 gitHash=$(curl ${GITHUB_TOKEN:+" -u \":$GITHUB_TOKEN\""} "https://api.github.com/repos/renovatebot/renovate/git/ref/tags/$latestTag" | jq -s '.[0].object.sha' | tr -d '"')
 
+if [[ "$UPDATE_NIX_OLD_VERSION" == "$latestTag" ]]; then
+    echo 'Version did not change, skipping...'
+    exit 0
+fi
+
 pushd "$tempDir"
 # renovate is a bit weird, and expects the full repo to be there to execute some commands.
 # --package-lock-only didn't work, so we do a git clone :(
@@ -26,3 +31,5 @@ sed -E 's#\bgitHash = ".*?"#gitHash = "'"${gitHash:0:7}"'"#' -i package.nix
 sed -E 's#\bnpmDepsHash = ".*?"#npmDepsHash = "'"$npmDepsHash"'"#' -i package.nix
 cp "$tempDir/package-lock.json" ./package-lock.json
 popd
+
+echo "Update successful, new version: $latestTag"
